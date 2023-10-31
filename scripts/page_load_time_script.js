1d4262c321534deaa4fe84e88a4f389f05756dcc
Mark <i.r.mark14@gmail.com>
2023-10-31 12:23:46 +0300

    feat: (interactivity added)

---
+++ scripts/page_load_time_script.js
@@ -0,0 +1,11 @@
+(function () {
+    window.addEventListener('load', function () {
+        let time = window.performance.timing
+        let pageLoadTime = time.loadEventStart - time.navigationStart;
+        let footer = document.querySelector('footer');
+        let line = document.createElement('p');
+        line.innerText = 'Страница загружена за ' + pageLoadTime + ' мс';
+        line.classList.add('homepage__load__info')
+        footer.appendChild(line);
+    });
+}());
